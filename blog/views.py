c4bfb64e2ed93d2069709067779385ac27d775c4
forshev <xdannyxwalkerx@gmail.com>
2016-06-02 19:59:56 +0500

    Added ckeditor.

--- blog/views.py
+++ blog/views.py
@@ -5,9 +5,19 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
+from django.template import RequestContext
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    paginator = Paginator(posts, 10)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(request, 'blog/post_list.html', {'posts' : posts})
 
 def post_detail(request, pk):
