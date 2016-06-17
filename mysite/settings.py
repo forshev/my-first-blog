e0b64f1f6c18ecc1c1dee427e3552bf32f1a2cbd
forshev <xdannyxwalkerx@gmail.com>
2016-06-17 17:51:41 +0500

    Added new templates.

--- mysite/settings.py
+++ mysite/settings.py
@@ -107,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'en-US'
 
 TIME_ZONE = 'Asia/Yekaterinburg'
 
